a3927214176ad80878303896ee3e3c3fa864b8b2
Prakhar Vikram Singh <prakharvikramsingh811@hotmail.com>
2025-11-01 00:08:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const session = require('express-session');
 const bcrypt = require('bcryptjs');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // ✅ Middleware setup
 app.use(express.static(path.join(__dirname, 'public')));
